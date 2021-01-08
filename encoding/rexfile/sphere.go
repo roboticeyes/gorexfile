2b4aca6a95af45838f61fdde8e242a112602ae4d
Bernhard Reitinger <br@rexos.org>
2021-01-08 16:32:06 +0100

    add prototype code for sphere

---
+++ encoding/rexfile/sphere.go
@@ -0,0 +1,92 @@
+package rexfile
+
+import (
+	"math"
+
+	"github.com/g3n/engine/gls"
+	"github.com/g3n/engine/math32"
+	"github.com/go-gl/mathgl/mgl32"
+)
+
+// NewSphere returns a new sphere (unit meters)
+func NewSphere(id, matID uint64, size float32) (Mesh, Material) {
+
+	phiStart, phiLength, thetaStart, thetaLength := 0, math.Pi*2, 0, math.Pi
+
+	thetaEnd := thetaStart + thetaLength
+	vertexCount := (widthSegments + 1) * (heightSegments + 1)
+
+	// Create buffers
+	positions := math32.NewArrayF32(vertexCount*3, vertexCount*3)
+	normals := math32.NewArrayF32(vertexCount*3, vertexCount*3)
+	uvs := math32.NewArrayF32(vertexCount*2, vertexCount*2)
+	indices := math32.NewArrayU32(0, vertexCount)
+
+	index := 0
+	vertices := make([][]uint32, 0)
+	var normal math32.Vector3
+
+	for y := 0; y <= heightSegments; y++ {
+		verticesRow := make([]uint32, 0)
+		v := float64(y) / float64(heightSegments)
+		for x := 0; x <= widthSegments; x++ {
+			u := float64(x) / float64(widthSegments)
+			px := -radius * math.Cos(phiStart+u*phiLength) * math.Sin(thetaStart+v*thetaLength)
+			py := radius * math.Cos(thetaStart+v*thetaLength)
+			pz := radius * math.Sin(phiStart+u*phiLength) * math.Sin(thetaStart+v*thetaLength)
+			normal.Set(float32(px), float32(py), float32(pz)).Normalize()
+
+			positions.Set(index*3, float32(px), float32(py), float32(pz))
+			normals.SetVector3(index*3, &normal)
+			uvs.Set(index*2, float32(u), float32(v))
+			verticesRow = append(verticesRow, uint32(index))
+			index++
+		}
+		vertices = append(vertices, verticesRow)
+	}
+
+	for y := 0; y < heightSegments; y++ {
+		for x := 0; x < widthSegments; x++ {
+			v1 := vertices[y][x+1]
+			v2 := vertices[y][x]
+			v3 := vertices[y+1][x]
+			v4 := vertices[y+1][x+1]
+			if y != 0 || thetaStart > 0 {
+				indices.Append(v1, v2, v4)
+			}
+			if y != heightSegments-1 || thetaEnd < math.Pi {
+				indices.Append(v2, v3, v4)
+			}
+		}
+	}
+
+	s.SetIndices(indices)
+	s.AddVBO(gls.NewVBO(positions).AddAttrib(gls.VertexPosition))
+	s.AddVBO(gls.NewVBO(normals).AddAttrib(gls.VertexNormal))
+	s.AddVBO(gls.NewVBO(uvs).AddAttrib(gls.VertexTexcoord))
+
+	r := float32(radius)
+
+	// Update bounding sphere
+	s.boundingSphere.Radius = 3
+	s.boundingSphereValid = true
+
+	// Update bounding box
+	s.boundingBox = math32.Box3{math32.Vector3{-r, -r, -r}, math32.Vector3{r, r, r}}
+	s.boundingBoxValid = true
+
+	return s
+
+	mesh := Mesh{
+		ID:         id,
+		Name:       "Sphere",
+		Coords:     getCoords(scale),
+		Triangles:  getTriangles(),
+		MaterialID: matID,
+	}
+
+	mat := NewMaterial(matID)
+	mat.KdRgb = mgl32.Vec3{0.9, 0.7, 0.1}
+
+	return mesh, mat
+}
