8dc38d829e7cc42890ee04f75cbcb5bc7df6309c
Elisabeth Reitinger <elisabeth.reitinger@robotic-eyes.com>
2021-05-06 17:33:44 +0200

    Add method to create a new quader with given size

--- encoding/rexfile/quader.go
+++ encoding/rexfile/quader.go
@@ -4,19 +4,14 @@ import (
 	"github.com/go-gl/mathgl/mgl32"
 )
 
-// NewQuader returns a new cube with size (meters)
+// NewQuader returns a new quader with given size
 func NewQuader(id, matID uint64, sizeX, sizeY, sizeZ float32) (Mesh, Material) {
-
-	// default geometry is 2 meters
-	scaleX := sizeX / 2.0
-	scaleY := sizeY / 2.0
-	scaleZ := sizeZ / 2.0
 
 	mesh := Mesh{
 		ID:         id,
 		Name:       "Quader",
-		Coords:     getCoords(scaleX, scaleY, scaleZ),
-		Triangles:  getTriangles(),
+		Coords:     getQuaderCoords(sizeX, sizeY, sizeZ),
+		Triangles:  getQuaderTriangles(),
 		MaterialID: matID,
 	}
 
@@ -26,50 +21,50 @@ func NewQuader(id, matID uint64, sizeX, sizeY, sizeZ float32) (Mesh, Material) {
 	return mesh, mat
 }
 
-func getCoords(scaleX, scaleY, scaleZ float32) []mgl32.Vec3 {
+func getQuaderCoords(sizeX, sizeY, sizeZ float32) []mgl32.Vec3 {
 
 	return []mgl32.Vec3{
-		mgl32.Vec3{scaleX * -1.0, scaleY * -1.0, scaleZ * 1.0},
-		mgl32.Vec3{scaleX * 1.0, scaleY * -1.0, scaleZ * 1.0},
-		mgl32.Vec3{scaleX * 1.0, scaleY * 1.0, scaleZ * 1.0},
-		mgl32.Vec3{scaleX * -1.0, scaleY * 1.0, scaleZ * 1.0},
-		mgl32.Vec3{scaleX * -1.0, scaleY * 1.0, scaleZ * 1.0},
-		mgl32.Vec3{scaleX* 1.0, scaleY * 1.0, scaleZ * 1.0},
-		mgl32.Vec3{scaleX * 1.0, scaleY * 1.0, scaleZ * -1.0},
-		mgl32.Vec3{scaleX * -1.0, scaleY * 1.0, scaleZ * -1.0},
-		mgl32.Vec3{scaleX * 1.0, scaleY * -1.0, scaleZ * -1.0},
-		mgl32.Vec3{scaleX * -1.0, scaleY * -1.0, scaleZ * -1.0},
-		mgl32.Vec3{scaleX * -1.0, scaleY * 1.0, scaleZ * -1.0},
-		mgl32.Vec3{scaleX* 1.0, scaleY * 1.0, scalez * -1.0},
-		mgl32.Vec3{scaleX * -1.0, scaleY * -1.0, scaleZ * -1.0},
-		mgl32.Vec3{scaleX * 1.0, scaleY * -1.0, scaleZ * -1.0},
-		mgl32.Vec3{scaleX * 1.0, scaleY * -1.0, scaleZ * 1.0},
-		mgl32.Vec3{scaleX * -1.0, scaleY * -1.0, scaleZ * 1.0},
-		mgl32.Vec3{scaleX * -1.0, scaleY * -1.0, scaleZ * -1.0},
-		mgl32.Vec3{scaleX * -1.0, scaleY * -1.0, scaleZ * 1.0},
-		mgl32.Vec3{scaleX * -1.0, scaleY * 1.0, scaleZ * 1.0},
-		mgl32.Vec3{scaleX * -1.0, scaleY * 1.0, scaleZ * -1.0},
-		mgl32.Vec3{scaleX * 1.0, scaleY * -1.0, scaleZ * 1.0},
-		mgl32.Vec3{scaleX * 1.0, scaleY * -1.0, scaleZ * -1.0},
-		mgl32.Vec3{scaleX * 1.0, scaleY * 1.0, scaleZ * -1.0},
-		mgl32.Vec3{scaleX * 1.0, scaleY * 1.0, scaleZ * 1.0},
+		{sizeX * 0.0, sizeY * 0.0, sizeZ * 1.0},
+		{sizeX * 1.0, sizeY * 0.0, sizeZ * 1.0},
+		{sizeX * 1.0, sizeY * 1.0, sizeZ * 1.0},
+		{sizeX * 0.0, sizeY * 1.0, sizeZ * 1.0},
+		{sizeX * 0.0, sizeY * 1.0, sizeZ * 1.0},
+		{sizeX * 1.0, sizeY * 1.0, sizeZ * 1.0},
+		{sizeX * 1.0, sizeY * 1.0, sizeZ * 0.0},
+		{sizeX * 0.0, sizeY * 1.0, sizeZ * 0.0},
+		{sizeX * 1.0, sizeY * 0.0, sizeZ * 0.0},
+		{sizeX * 0.0, sizeY * 0.0, sizeZ * 0.0},
+		{sizeX * 0.0, sizeY * 1.0, sizeZ * 0.0},
+		{sizeX * 1.0, sizeY * 1.0, sizeZ * 0.0},
+		{sizeX * 0.0, sizeY * 0.0, sizeZ * 0.0},
+		{sizeX * 1.0, sizeY * 0.0, sizeZ * 0.0},
+		{sizeX * 1.0, sizeY * 0.0, sizeZ * 1.0},
+		{sizeX * 0.0, sizeY * 0.0, sizeZ * 1.0},
+		{sizeX * 0.0, sizeY * 0.0, sizeZ * 0.0},
+		{sizeX * 0.0, sizeY * 0.0, sizeZ * 1.0},
+		{sizeX * 0.0, sizeY * 1.0, sizeZ * 1.0},
+		{sizeX * 0.0, sizeY * 1.0, sizeZ * 0.0},
+		{sizeX * 1.0, sizeY * 0.0, sizeZ * 1.0},
+		{sizeX * 1.0, sizeY * 0.0, sizeZ * 0.0},
+		{sizeX * 1.0, sizeY * 1.0, sizeZ * 0.0},
+		{sizeX * 1.0, sizeY * 1.0, sizeZ * 1.0},
 	}
 }
 
-func getTriangles() []Triangle {
+func getQuaderTriangles() []Triangle {
 
 	return []Triangle{
-		Triangle{V0: 0, V1: 1, V2: 2},
-		Triangle{V0: 2, V1: 3, V2: 0},
-		Triangle{V0: 4, V1: 5, V2: 6},
-		Triangle{V0: 6, V1: 7, V2: 4},
-		Triangle{V0: 8, V1: 9, V2: 10},
-		Triangle{V0: 10, V1: 11, V2: 8},
-		Triangle{V0: 12, V1: 13, V2: 14},
-		Triangle{V0: 14, V1: 15, V2: 12},
-		Triangle{V0: 16, V1: 17, V2: 18},
-		Triangle{V0: 18, V1: 19, V2: 16},
-		Triangle{V0: 20, V1: 21, V2: 22},
-		Triangle{V0: 22, V1: 23, V2: 20},
+		{V0: 0, V1: 1, V2: 2},
+		{V0: 2, V1: 3, V2: 0},
+		{V0: 4, V1: 5, V2: 6},
+		{V0: 6, V1: 7, V2: 4},
+		{V0: 8, V1: 9, V2: 10},
+		{V0: 10, V1: 11, V2: 8},
+		{V0: 12, V1: 13, V2: 14},
+		{V0: 14, V1: 15, V2: 12},
+		{V0: 16, V1: 17, V2: 18},
+		{V0: 18, V1: 19, V2: 16},
+		{V0: 20, V1: 21, V2: 22},
+		{V0: 22, V1: 23, V2: 20},
 	}
 }
